db9e17569e499aeba890a3e4c0547b6f50beb4ee
war-in <marcin.warchol20@gmail.com>
2023-05-14 14:15:40 +0200

    format

--- backend/test/admin/TestManageRanks.js
+++ backend/test/admin/TestManageRanks.js
@@ -2,40 +2,39 @@ const { expect } = require("chai");
 const { BN } = require("@openzeppelin/test-helpers");
 
 const ManageRanks = artifacts.require("ManageRanks");
-const Ranks = artifacts.require("Ranks");
 
 contract("ManageRanks", function (accounts) {
-    let manageRanks;
+  let manageRanks;
 
-    const [admin_user] = accounts;
+  const [admin_user] = accounts;
 
-    beforeEach(async () => {
-        manageRanks = await ManageRanks.new({ from: admin_user });
-    });
+  beforeEach(async () => {
+    manageRanks = await ManageRanks.new({ from: admin_user });
+  });
 
-    it("should create ranks", async () => {
-        const name = "My Ranks";
-        const numberOfRanks = 3;
-        const ranksNames = ["Rank 1", "Rank 2", "Rank 3"];
-        const ranksSymbols = ["R1", "R2", "R3"];
-        const ranksPrices = [new BN("1"), new BN("2"), new BN("3")];
+  it("should create ranks", async () => {
+    const name = "My Ranks";
+    const numberOfRanks = 3;
+    const ranksNames = ["Rank 1", "Rank 2", "Rank 3"];
+    const ranksSymbols = ["R1", "R2", "R3"];
+    const ranksPrices = [new BN("1"), new BN("2"), new BN("3")];
 
-        await manageRanks.createRanks(
-            name,
-            numberOfRanks,
-            ranksNames,
-            ranksSymbols,
-            ranksPrices,
-            { from: admin_user }
-        );
+    await manageRanks.createRanks(
+      name,
+      numberOfRanks,
+      ranksNames,
+      ranksSymbols,
+      ranksPrices,
+      { from: admin_user }
+    );
 
-        const allRanks = await manageRanks.getAllRanksNames();
-        const ranksAddress = await manageRanks.getRanksContractAddress(name);
+    const allRanks = await manageRanks.getAllRanksNames();
+    const ranksAddress = await manageRanks.getRanksContractAddress(name);
 
-        expect(allRanks.length).to.equal(1);
-        expect(allRanks[0]).to.equal(name);
-        expect(ranksAddress).to.not.equal(
-            "0x0000000000000000000000000000000000000000"
-        );
-    });
+    expect(allRanks.length).to.equal(1);
+    expect(allRanks[0]).to.equal(name);
+    expect(ranksAddress).to.not.equal(
+      "0x0000000000000000000000000000000000000000"
+    );
+  });
 });
