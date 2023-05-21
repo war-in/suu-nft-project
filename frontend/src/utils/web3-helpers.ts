f089ddf6ba4edbb242a06118660dfce4d029edc0
Tomasz Boroń <tomasz.boron@swmansion.com>
2023-05-21 17:32:20 +0200

    Move contract data to env

--- frontend/src/utils/web3-helpers.ts
+++ frontend/src/utils/web3-helpers.ts
@@ -1,54 +1,67 @@
 import Web3 from "web3";
 import Contract from "web3-eth-contract";
 
-import AdminContract from 'contracts/Admin.json';
-import RankContract from 'contracts/Rank.json';
-import RanksContract from 'contracts/Ranks.json';
-import TicketContract from 'contracts/Ticket.json';
+import RankContract from "contracts/Rank.json";
+import RanksAdminContract from "contracts/ManageRanks.json";
+import RanksContract from "contracts/Ranks.json";
+import TicketContract from "contracts/Ticket.json";
+import TicketsAdminContract from "contracts/ManageTickets.json";
 
 export interface Contracts {
-    adminContract: Contract;
-    rankContract: Contract;
-    ranksContract: Contract;
-    ticketContract: Contract;
+  rankContract: Contract;
+  ranksAdmin: Contract;
+  ranksContract: Contract;
+  ticketContract: Contract;
+  ticketsAdmin: Contract;
 }
-
-const CONTRACT_PROVIDER_URL = 'http://localhost:8545';
 
 const ethereum = window.ethereum;
 //@ts-ignore
 const web3 = new Web3(ethereum);
 
 const formatContract = (contract: { [key: string]: any }) => {
-    return JSON.parse(JSON.stringify(contract));
-}
-
-/// TODO: Check if contract address is the last element in networks in contract's json file, for now addresses are hardcoded
-// const extractAddress = (contract: { [key: string]: any }, networkId: string) => {
-//     return contract.networks[networkId].address;
-// }
+  return JSON.parse(JSON.stringify(contract));
+};
 
 export const asciiToHex = (value: string) => {
-    return web3.utils.asciiToHex(value);
-}
+  return web3.utils.asciiToHex(value);
+};
 
 export const setupContracts = (): Contracts => {
-    Contract.setProvider(CONTRACT_PROVIDER_URL);
+  Contract.setProvider(process.env.REACT_APP_CONTRACT_PROVIDER_URL!);
 
-    const formattedAdminContract = formatContract(AdminContract);
-    const formattedRankContract = formatContract(RankContract);
-    const formattedRanksContract = formatContract(RanksContract);
-    const formattedTicketContract = formatContract(TicketContract);
+  const formattedRankContract = formatContract(RankContract);
+  const formattedRanksAdminContract = formatContract(RanksAdminContract);
+  const formattedRanksContract = formatContract(RanksContract);
+  const formattedTicketContract = formatContract(TicketContract);
+  const formattedTicketsAdminContract = formatContract(TicketsAdminContract);
 
-    const adminContract = new Contract(formattedAdminContract.abi, '0x0000000000000000000000000000000000000000');  /// TODO: Paste proper address
-    const rankContract = new Contract(formattedRankContract.abi, '0x0000000000000000000000000000000000000000');   /// TODO: Paste proper address
-    const ranksContract = new Contract(formattedRanksContract.abi, '0x25173feAFc2dBB9FA711d3A1680e932362e9cBc7');   /// TODO: Paste proper address
-    const ticketContract = new Contract(formattedTicketContract.abi, '0xFCFfC4B8f43A2129AE144A43B174882FfD34Ed1d');
+  const rankContract = new Contract(
+    formattedRankContract.abi,
+    process.env.REACT_APP_RANK_CONTRACT_ADDRESS
+  );
+  const ranksAdmin = new Contract(
+    formattedRanksAdminContract.abi,
+    process.env.REACT_APP_MANAGE_RANKS_CONTRACT_ADDRESS
+  );
+  const ranksContract = new Contract(
+    formattedRanksContract.abi,
+    process.env.REACT_APP_RANKS_CONTRACT_ADDRESS
+  );
+  const ticketContract = new Contract(
+    formattedTicketContract.abi,
+    process.env.REACT_APP_TICKET_CONTRACT_ADDRESS
+  );
+  const ticketsAdmin = new Contract(
+    formattedTicketsAdminContract.abi,
+    process.env.REACT_APP_MANAGE_TICKETS_CONTRACT_ADDRESS
+  );
 
-    return {
-        adminContract,
-        rankContract,
-        ranksContract,
-        ticketContract,
-    }
-}
+  return {
+    rankContract,
+    ranksAdmin,
+    ranksContract,
+    ticketContract,
+    ticketsAdmin,
+  };
+};
